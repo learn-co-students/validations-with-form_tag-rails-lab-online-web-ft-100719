2eab96527ae85e87c4afbb3a8c628af5c1a3ff19
Alyssa Yates <alyssayates@Alyssas-MacBook-Air.local>
2019-12-20 16:39:52 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :author, :post, only: [:show, :new, :edit, :create, :update]
 end
