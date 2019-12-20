044cf360dd9cbefb0f515b60be7ecd27be713159
github ginasekhar <github ginasekhar@yahoo.com>
2019-12-19 23:37:03 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :phone_number, numericality: { only_integer: true, 
+        message: "can only contain 10 digits"},
+        length: { is: 10 }
+
 end
