dd543ea54ef3478bd455a96a127559ae6542858b
Daniel Rooker <dannyrooks@gmail.com>
2020-01-07 15:22:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:new, :show, :create, :index, :edit, :update]
+  resources :posts, only: [:new, :show, :create, :index, :edit, :update]
+
 end
